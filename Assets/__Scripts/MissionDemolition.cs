9be49981fc7f7380fef0b66a9c063a5095519ff3
KaJuan Jones <47114033+kajuanjones@users.noreply.github.com>
2019-09-17 10:24:59 -0400

    MissionDemo code
    Working on MissionDemo code

--- Assets/__Scripts/MissionDemolition.cs
+++ Assets/__Scripts/MissionDemolition.cs
@@ -5,39 +5,95 @@ using UnityEngine.UI;
 
 public enum GameMode
 {
+    idle,
+    playing,
+    levelEnd
+
 
 
 }
 
-public class MissionDemolition : MonoBehaviour {
+public class MissionDemolition : MonoBehaviour
+{
     static private MissionDemolition S; // a private Singleton
 
     [Header("Set in Inspector")]
 
+    public Text uitLevel;
+    public Text uitShots;
+    public Text uitButton;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
 
     [Header("Set Dynamically")]
 
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start()
     {
+        S = this;
 
+        level = 0;
+        levelMax = castles.Length;
+        StartLevel();
     }
-	
-	void StartLevel()
+
+    void StartLevel()
     {
         // Get rid of the old castle if one exists
- 
+        if (castle != null)
+        {
+            Destroy(castle);
+        }
+
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach (GameObject pTemp in gos)
+        {
+            Destroy(pTemp);
+        }
+
+        castle = Instantiate<GameObject>(castles[level]);
+        castle.transform.position = castlePos;
+        shotsTaken = 0;
+
+        SwitchView("Show Both");
+        ProjectileLine.S.Clear();
+
+        Goal.goalMet = false;
+
+        UpdateGUI();
+
+        mode = GameMode.playing;
+
+
+
+
     }
 
     void UpdateGUI()
     {
-
+        uitLevel.text = "Level:" + (level + 1) + "of" + levelMax;
+        uitShots.text = "Shots Taken" + shotsTaken;
     }
 
     void Update()
     {
+        UpdateGUI();
 
+        if( (mode == GameMode.playing) && Goal.goalMet)
+        {
+            mode = GameMode.levelEnd;
+            SwitchView("Show Both");
+            Invoke("NextLevel", 2f);
+        }
 
     }
 
